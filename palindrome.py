7d9b671468f8884688930eb71324f4547881528f
Sarthak Garud <garudsarthak00@gmail.com>
2025-07-06 11:56:24 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,7 +3,7 @@ def is_palindrome(s):
     return s == s[::-1]
 
 # Example usage:
-word = input("Enter a word: ")
+# word = input("Enter a word: ")
 if is_palindrome(word):
     print("It's a palindrome!")
 else:
